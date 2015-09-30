cc3b178a3ae313a51a67a2749102d6d517d6d8ff
Loveland Nuts & Bolts <robotics5040@gmail.com>
2015-09-30 09:26:36 -0400

    Update with a demo program and a new autonomous program
    Robot drives forward for 4 seconds

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -83,6 +83,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("K9IrSeeker", K9IrSeeker.class);
     manager.register("K9Line", K9Line.class);
     manager.register("MtrTest", MtrTest.class);
+    manager.register("AutoMtrTest", AutoMtrTest.class);
 
     /*
      * The following example op modes are designed to work with a pushbot-style robot.
@@ -98,6 +99,8 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("PushBotDriveTouch", PushBotDriveTouch.class);
     manager.register("PushBotIrSeek", PushBotIrSeek.class);
     manager.register("PushBotSquare", PushBotSquare.class);
+    manager.register("Demo", Demo.class);
+
 
   }
 }
